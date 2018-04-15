b45fb826d16f36007bae9dadd6ecbc0b9f0ac81a
Mihai Bucur <a.mihai.bucur@gmail.com>
2018-04-15 23:05:13 +0300

    Add constants

---
+++ constants.py
@@ -0,0 +1,3 @@
+PATH_TO_PROJECTS = '/var/lib/docker/volumes/everythingHappensHere/_data/projects'
+PATH_TO_ARGUMENTS = '/var/lib/docker/volumes/everythingHappensHere/_data/arguments'
+PATH_TO_EXPECTED_RESULTS = '/var/lib/docker/volumes/everythingHappensHere/_data/expectedResults'
